8eed294b25679a2019efcfba76fd7d8fb1b7336a
DONGWOOK LEE <cpawookie@gmail.com>
2018-09-17 04:07:25 +0900

    #2 Setting Typescript Up

---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "Wooqii",
+    age = 32,
+    gender = "male";
+
+const sayHi = {name, age, gender} => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
