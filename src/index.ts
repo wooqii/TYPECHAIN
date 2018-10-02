3b8c07a388967204a972e6680c531d2228499215
DONGWOOK LEE <cpawookie@gmail.com>
2018-10-02 18:04:30 +0900

    #4 Types in typscript
    lesson4

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 const name = "Wooqii",
-  age = 31,
-  gender = "male";
+      age = 31,
+      gender = "male";
 
 const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
 };
 
 console.log(sayHi("Wooqii", 31, "male"));
