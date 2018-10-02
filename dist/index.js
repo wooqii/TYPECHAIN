5b74a6f77799f5e1c1346689cf0a961e75e69a17
DONGWOOK LEE <cpawookie@gmail.com>
2018-10-02 18:09:16 +0900

    #4 Types in typescript

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Wooqii", age = 31, gender = "male";
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+};
+console.log(sayHi("Wooqii", 31, "male"));
+//# sourceMappingURL=index.js.map
