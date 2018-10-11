6a1868b31bde4798a48089bfa4196d020e1d7feb
DONGWOOK LEE <cpawookie@gmail.com>
2018-10-11 17:14:53 +0900

    #9 Creating a Block pt.1
    Creating a block with typscript

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,46 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public Hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
-  constructor( 
+
+  static calculateBlockHash = (
+    index: number,
+    Hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + Hash + data + timestamp).toString();
+
+  constructor(
     index: number,
     Hash: string,
     previousHash: string,
     data: string,
     timestamp: number
   ) {
-  this.index = index;
-  this.Hash = Hash;
-  this.previousHash = previousHash;
-  this.data = data;
-  this.timestamp = timestamp;
+    this.index = index;
+    this.Hash = Hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const genesisBlock:Block = new Block(0, "2083236893", "", "WooqiiBlock", 20181010);
 
-let blockchain: [Block] = [genesisBlock];
 
-console.log(blockchain);
+const genesisBlock: Block = new Block( 0,  "2083236893",  "", "WooqiiBlock",  20181010);
+
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000)
 
 export {};
